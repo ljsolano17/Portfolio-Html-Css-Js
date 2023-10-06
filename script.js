85ec48ae046a4f6bcfcb72bc39489bace999756e
ljsolano17 <ljsolano17@hotmail.com>
2023-10-06 16:29:40 -0600

    Add Profile

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Target elements to open class*/ 
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
